e0cbe9d854fc660787f770c19e1f313605cd3511
Alyssa Pagulayan <99alyssapagulayan@gmail.com>
2023-05-03 14:19:15 +0100

    mockup

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var form = document.querySelector('form');
+var todoList = documen.querySelector('ul');
+var button = document.querySelector('button');
+var input = document.getElementById('user-todo');
